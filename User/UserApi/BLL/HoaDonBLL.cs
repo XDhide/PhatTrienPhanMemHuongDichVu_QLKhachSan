314e39a3e8402a6edc91d25a408684ec05d0c773
Dung <Dung@DESKTOP-3GIS5H1>
2025-10-13 21:29:27 +0700

    Update main to match restructure

---
+++ User/UserApi/BLL/HoaDonBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data;
+using HotelManagement.Module;
+using DAL;
+
+namespace BLL
+{
+    public class HoaDonBLL
+    {
+        private HoaDonDAL _dal;
+
+        public HoaDonBLL()
+        {
+            _dal = new HoaDonDAL();
+        }
+
+        public DataTable GetAll()
+        {
+            return _dal.GetAll();
+        }
+
+        public DataTable GetById(int maHD)
+        {
+            return _dal.GetById(maHD);
+        }
+
+        public string Them(HoaDon obj)
+        {
+            if (obj == null)
+                return "Thông tin HoaDon không hợp lệ";
+            return _dal.Them(obj);
+        }
+
+        public string Sua(HoaDon obj)
+        {
+            if (obj == null)
+                return "Thông tin HoaDon không hợp lệ";
+            return _dal.Sua(obj);
+        }
+
+        public string Xoa(int maHD)
+        {
+            return _dal.Xoa(maHD);
+        }
+    }
+}
